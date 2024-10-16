fac1aaf1ee64e0ed7d86d3f38e635ce6d6dd192d
admin <valery.muhanin@yandex.ru>
2024-10-17 00:32:59 +0300

    ВНЕСЕНЫ ИЗМИНЕНИЯ

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     private static Object system;
 
     public static void main(String[] args) {
-        System.out.println("ПРИВЕТ МИР!");
-        System.out.println("HELLO WORLD!");
+        System.out.println("ПРИВЕТ МИР!!!");
+        System.out.println("HELLO WORLD!!!");
     }
 
 }
